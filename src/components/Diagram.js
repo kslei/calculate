8ec1817977033f48bc5c2eb55e18275359e391a8
Sergey <koolchitsky@gmail.com>
2023-02-04 16:30:25 +0200

    deploy-mobile

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import InputRadio from '../forms/InputRadio';
 import styles from '../styles/components/Diagram.module.scss';
 
-const Diagram = ({provider, storage, transfer}) => {
+const Diagram = ({i, provider, storage, transfer, color, setSum}) => {
+  
   let defaultStoragePrice;
   Array.isArray(provider.storagePrice) ? defaultStoragePrice = provider.storagePrice[0].price : defaultStoragePrice = provider.storagePrice;
   
@@ -10,7 +11,11 @@ const Diagram = ({provider, storage, transfer}) => {
   let storageP;
   let transferP;
   let sum;
-    
+  
+  useEffect(()=>{
+    setSum(i, sum)
+  }, [storage, transfer, storagePrice, sum])
+
   if (provider.storageMin) {
     if (storage <= provider.storageMin) {
       storageP = 0;
@@ -33,13 +38,20 @@ const Diagram = ({provider, storage, transfer}) => {
   sum = storageP + transferP;
   if (provider.minPayment && sum <= provider.minPayment) { sum = provider.minPayment };
   if (provider.maxPayment && sum >= provider.maxPayment) { sum = provider.maxPayment };
-  let res = Math.floor(sum * 100) / 100
- 
+
+  let res = Math.floor(sum * 100) / 100;
+
+  let diaStyle;
+  if (window.innerWidth > 768 && color) {diaStyle = { width: sum + '%' , backgroundColor: provider.colorMin}};
+  if (window.innerWidth > 768 && !color) {diaStyle = { width: sum + '%'}};
+  if (window.innerWidth <= 768 && color) {diaStyle = { height: sum + '%' , backgroundColor: provider.colorMin}};
+  if (window.innerWidth <= 768 && !color) {diaStyle = { height: sum + '%'}};
+
   return (
     <div className={styles.diagram}>
-      <div className={styles.diagram__chart}>
-        <div className={styles.diagram__chart_dia} style = {window.innerWidth > 768 ? {width: sum +'%'} : {height: sum +'%'}}></div>
-        <div className={styles.diagram__chart_value}>${res}</div>
+      <div className={styles.diagram__chart} >
+        <div className={styles.diagram__chart_dia} style = {diaStyle}></div>
+        <div className={styles.diagram__chart_value}> ${res}</div>
       </div>
       <div className={styles.diagram__icon} style={{background: provider.background}}>
         <img src={provider.img} alt="#"/>
--- src/components/Diagrams.js
+++ src/components/Diagrams.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Diagram from './Diagram';
 import {provider} from '../UI/data';
 import styles from '../styles/components/Diagrams.module.scss';
 
 const Diagrams = ({storage, transfer}) => {
-  
+  const [price, setPrice] = useState([])
+  const [color, setColor] = useState(0)
+    
+  var arr = price;
+  const setSum =(i, sum) =>{
+    arr[i] = sum;
+    setPrice(arr);
+    let min = Math.min.apply(window, arr);
+    setColor(arr.indexOf(min));
+  }
+      
   return (
     <div className={styles.diagrams}>
       <div className={styles.wrapper}>
-        {provider.map(item =>
-          <Diagram key={item.name} provider={item} storage={storage} transfer={transfer} />
+        {provider.map((item, i) =>
+          <Diagram key={item.name} i={i} provider={item} storage={storage} transfer={transfer} color={color === i ? true : false} setSum={setSum}/>
         )}
       </div>
     </div>
